ddf9112f17e4e8b28bed14dc5897eed23954f9cd
Christopher Lee <leec@chem.ucla.edu>
2013-05-17 18:36:43 -0700

    added support for text search on stored content

--- reusabletext/mongo.py
+++ reusabletext/mongo.py
@@ -1,6 +1,7 @@
 import pymongo
 import jsonpickle
 import json
+import os.path
 try:
     from jinja2 import Template
 except ImportError:
@@ -32,11 +33,41 @@ def extract_formats(tree):
             d[tokens[1]] = '\n'.join(node.text)
     return d
         
+def combine_text(doc):
+    'combine text lists from a doc tree'
+    l = list(getattr(doc, 'text', ()))
+    for c in doc.children:
+        l += combine_text(c)
+    return l
+    
             
 
 def jsonize_docs(docs):
     'transform docs to JSON style dicts that mongodb can save'
-    return [json.loads(jsonpickle.encode(doc)) for doc in docs]
+    l = []
+    for doc in docs:
+        d = json.loads(jsonpickle.encode(doc)) # create a dictionary
+        d['content'] = '\n'.join(combine_text(doc)) # searchable text string
+        try: # get title attr or metadata
+            d['title'] = '\n'.join(doc.title)
+        except AttributeError:
+            m = []
+            for s in getattr(doc, 'metadata', ()):
+                if s.startswith(':title: '):
+                    m.append(s[8:])
+            if m:
+                d['title'] = '\n'.join(m)
+        l.append(d)
+    return l
+
+def init_collection(dbName='socraticqs', collName='latest', **kwargs):
+    client = pymongo.MongoClient(**kwargs)
+    db = client[dbName]
+    db.drop_collection(collName)
+    coll = db[collName]
+    coll.create_index([('title', 'text'), ('content', 'text')],
+                      weights=dict(title=10, content=1))
+    return coll
 
 def get_collection(dbName='socraticqs', collName='latest', **kwargs):
     client = pymongo.MongoClient(**kwargs)
@@ -58,6 +89,24 @@ def insert_formats(formatDict, coll=None, collName='formats', **kwargs):
     l = [dict(_id=t[0], template=t[1]) for t in formatDict.items()]
     coll.insert(l)
 
+def text_search(coll, query, limit=50):
+    d = coll.database.command('text', coll.name, search=query,
+                              projection=dict(content=False), limit=limit)
+    l = []
+    for docDict in d['results']:
+        o = unpickle_json(docDict['obj'])
+        l.append((o, docDict['score']))
+    return l
+
+def unpickle_json(d):
+    _id = d['_id']
+    del d['_id']
+    s = json.dumps({'py/object':d['py/object'], 'py/state':d['py/state']})
+    o = jsonpickle.decode(s)
+    o._id = _id
+    return o
+
+
 class DocIDIndex(object):
     'dict interface to doc collection in mongodb'
     def __init__(self, coll=None, **kwargs):
@@ -68,13 +117,7 @@ class DocIDIndex(object):
         d = self.coll.find_one({'py/state.tokenID':k})
         if d is None:
             raise KeyError('ID %s not found' % k)
-        _id = d['_id']
-        del d['_id']
-        s = json.dumps(d)
-        o = jsonpickle.decode(s)
-        o._id = _id
-        return o
-
+        return unpickle_json(d)
 
 class FormatIndex(object):
     'dict interface to formats stored in mongodb'
@@ -103,10 +146,16 @@ def get_indexes(srcpath):
 def save_rust_repo(srcpath, **kwargs):
     'save rust and formats to database'
     import parse
-    srcfiles = parse.find_source_files(srcpath)
+    path = os.path.join(srcpath, 'sourcefiles.txt')
+    with open(path, 'rU') as ifile:
+        srcfiles = [os.path.join(srcpath, s.strip()) for s in ifile]
     tree = parse.parse_files(srcfiles)
     docs = extract_docs(tree)
     insert_docs(docs, **kwargs)
+    print 'inserted %d documents' % len(docs)
+    path = os.path.join(srcpath, 'formats.rst')
+    tree = parse.parse_files([path])
     formats = extract_formats(tree)
     insert_formats(formats, **kwargs)
+    print 'inserted %d formats' % len(formats)
 
